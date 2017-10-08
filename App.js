8b799f1f0a6d01be3ccbf2fe5a2599617870ecc3
Stavros Papadopoulos <stavrospap96@gmail.com>
2017-10-09 00:36:58 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BusinessList from './components/BusinessList';
+import SearchBar from './components/SearchBar'
+
+class App extends Component {
+  render() {
+    return (<div className="App">
+  <h1>ravenous</h1>
+   <SearchBar/>
+  <BusinessList />
+</div>
+    );
+  }
+}
+
+export default App;
---
+++ Business.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './Business.css';
+const business={
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+class Business extens React.Component{
+  render(){
+    return(<div className="Business">
+  <div className="image-container">
+    <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+  </div>
+  <h2>MarginOtto Pizzeria</h2>
+  <div className="Business-information">
+    <div className="Business-address">
+      <p>{bussiness.address}</p>
+      <p>{business.city}</p>
+      <p>{business.state} {business.zipCode}</p>
+    </div>
+    <div className="Business-reviews">
+      <h3>bussiness.category</h3>
+      <h3 className="rating">4.5 stars}</h3>
+      <p>{business.reviewCount} reviews</p>
+    </div>
+  </div>
+</div>);
+  }
+}
+export default Business;
